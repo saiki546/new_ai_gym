5c252ad0459d06cf96b056c37db26fc2c418eecb
saiki546 <132424125+saiki546@users.noreply.github.com>
2026-07-25 11:18:12 +0530

    Change WebRtcMode from SENDONLY to RECVONLY

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from streamlit_webrtc import webrtc_streamer, WebRtcMode
 
 ctx = webrtc_streamer(
     key="test",
-    mode=WebRtcMode.SENDONLY,
+    mode=WebRtcMode.RECVONLY,
 )
 
 st.write(ctx.state.playing)
